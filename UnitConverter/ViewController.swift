c48e6a8fe10dddc9ee7166c75e5564cb40f4eeff
Kohei Yamamoto <kh2yam1@gmial.com>
2019-07-25 13:19:09 -0400

    Initial Commit

---
+++ UnitConverter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UnitConverter
+//
+//  Created by Kohei Yamamoto on 2019/07/25.
+//  Copyright © 2019 Kohei YAMAMOTO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
