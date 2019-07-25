7e846cf607aeccfab24bf6ad4129eafce04a5161
Kohei Yamamoto <kh2yam1@gmial.com>
2019-07-25 14:30:59 -0400

    Implement oz to ml converter

--- UnitConverter/ViewController.swift
+++ UnitConverter/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
+    @IBOutlet weak var ozTextField: UITextField!
+    @IBOutlet weak var mlResultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    // MARK: Actions
+    @IBAction func convertOzToMl(_ sender: Any) {
+        let oneOz:Float = 29.5735 //ml
+        if ozTextField.text != nil {
+            if let oz:Float = Float(ozTextField.text!) {
+                let ml = oz*oneOz
+                mlResultLabel.text = String(ml)
+            } else {
+                mlResultLabel.text = "NA"
+            }
+        } else {
+            mlResultLabel.text = "NA"
+        }
+    }
+    
 }
 
